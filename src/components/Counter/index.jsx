e1ca9d2ad22ab5327c92f923c4d2294ad946f207
jvscirilo <jvscirilo12@gmail.com>
2022-09-03 01:25:12 -0300

    add: max, min and alpha field

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,23 +1,64 @@
 import { useState } from 'react';
-import componentDefaultStyle from '../../styles/global';
 
 function Counter () {
 
-    const [count, setCounter ] = useState(0);
+    const [count, setCount] = useState(0);
+    const [alpha, setAlfa] = useState(1);
+    const MAX_COUNT_VALUE = 1000;
+    const MIN_COUNT_VALUE = -1000;
+
+    function handleIncreaseCount() {
+        if (canIncrease()) setCount(count + alpha);
+    }
+    
+    function handleDecreaseCount() {
+        if (canDecrease()) setCount(count - alpha);
+    }
+    
+    function handleChangeAlfa(element) {
+        setAlfa(parseInt(element.target.value));
+    }
+
+    function canIncrease () {
+        return ((count + alpha) <= MAX_COUNT_VALUE); 
+    }
+    
+    function canDecrease() {
+        return ((count - alpha) >= MIN_COUNT_VALUE);
+    }
 
     return (
-        <div style={componentDefaultStyle}>
-            
+        <div style={{
+            border: 'solid 1px gray',
+            paddingInline: '1.5rem',
+            paddingBottom: '1.5rem'
+        }}>
             <h2> Counter </h2> 
-            
-            <span><code>useState</code></span>
-            
+            <span>
+                <code>useState</code>
+            </span>
             <hr />
-
-            <p> counter value: <strong>{ count } </strong></p>
-
-            <button onClick={() => setCounter(count => count + 1)}> Add </button>
-            <button onClick={() => setCounter(count => count - 1)}> remove </button>
+            <p> 
+                value: <strong> {count}</strong>
+                <br />
+                <small>(max: {MAX_COUNT_VALUE} min: {MIN_COUNT_VALUE})</small>
+                <br />            
+            </p>
+            <label htmlFor="alpha">
+                alpha: <input 
+                        max={10}
+                        min={1} 
+                        defaultValue={1} 
+                        name="alpha" 
+                        type="number" 
+                        placeholder="alpha" 
+                        onChange={e => handleChangeAlfa(e)}
+                        title={"set a value beetwen (" + MIN_COUNT_VALUE + ") and " + MAX_COUNT_VALUE}
+                        />
+            </label>
+            <br />
+            <br />
+            <button onClick={handleIncreaseCount}> increase </button> or <button onClick={handleDecreaseCount}> decrease </button> by alpha
         </div>
     );
 }
