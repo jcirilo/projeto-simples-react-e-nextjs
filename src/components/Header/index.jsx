80797415cbc12077bb8d8c164c6d83bafc7d70a8
jvscirilo <jvscirilo12@gmail.com>
2022-09-03 01:26:03 -0300

    refactory: removed external stylesheet link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,20 @@
 function Header(props) {
     return (
-        <div>
-            <h1>Header</h1>
-            <hr />
-        </div>
+        <header style={{
+            borderBottom: 'solid 1px gray',
+            margin: 0,
+            width: '100%',
+        }}>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                width: 'min(100%, 600px)',
+                marginInline: 'auto',
+            }}>
+                <h1> Next.js App</h1>
+                <small style={{marginLeft: '.4rem'}}> by <a style={{textDecoration: 'none', color: 'black'}} href="https://github.com/jcirilo/projeto-simples-react-e-nextjs" target="_blank">github.com/jcirilo</a></small>
+            </div>
+        </header>
     )
 }
 
